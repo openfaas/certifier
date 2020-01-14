0bb6c4d3f7dbab59ae0bac64be5c7f39cd6ddbbb
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-01-14 20:18:39 +0000

    Form URL with net.URL
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- tests/health_test.go
+++ tests/health_test.go
@@ -2,19 +2,29 @@ package tests
 
 import (
 	"net/http"
+	"net/url"
 	"os"
 	"testing"
 )
 
 func Test_HealthEndpoint(t *testing.T) {
-	_, res, err := httpReq(os.Getenv("gateway_url")+"/healthz", http.MethodGet, nil)
+
+	gwURL, urlErr := url.Parse(os.Getenv("gateway_url"))
+
+	if urlErr != nil {
+		t.Fatal(urlErr)
+	}
+
+	gwURL.Path = "/healthz"
+
+	_, res, err := httpReq(gwURL.String(), http.MethodGet, nil)
 	if err != nil {
 		t.Log(err)
 		t.Fail()
 	}
 
 	if res.StatusCode != http.StatusOK {
-		t.Logf("got %d, wanted %d", res.StatusCode, http.StatusOK)
+		t.Logf("error with /healthz, got %d, but want %d", res.StatusCode, http.StatusOK)
 		t.Fail()
 	}
 }
